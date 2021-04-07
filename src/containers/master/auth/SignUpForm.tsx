c02d37d636138fb839412d2b81672b76f1c0e1e9
Napsztar <napsztar@gmail.com>
2021-04-07 13:50:46 +0900

    implement SignUP

--- src/containers/master/auth/SignUpForm.tsx
+++ src/containers/master/auth/SignUpForm.tsx
@@ -3,15 +3,26 @@ import { ChangeEvent, FormEvent, useEffect } from 'react';
 import {
   masterChangeField,
   masterInitializeForm,
+  masterSignUpThunk,
 } from '../../../modules/master/auth';
 import { RootState } from '../../../modules';
 import AuthForm from '../../../components/master/auth/AuthForm';
 
 export default function SignUpForm() {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ masterAuth }: RootState) => ({
+  const {
+    form,
+    data: auth,
+    loading: authLoading,
+    error: authError,
+  } = useSelector(({ masterAuth, masterAuthAsync }: RootState) => ({
     form: masterAuth.signUp,
+    data: masterAuthAsync.auth.data,
+    loading: masterAuthAsync.auth.loading,
+    error: masterAuthAsync.auth.error,
   }));
+
+  const loading = authLoading;
 
   const onChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { value, name } = e.target;
@@ -25,12 +36,28 @@ export default function SignUpForm() {
   };
   const onSubmit = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    const { email, username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      return;
+    }
+    dispatch(masterSignUpThunk({ email, username, password, passwordConfirm }));
   };
 
   useEffect(() => {
     dispatch(masterInitializeForm('singUp'));
   }, [dispatch]);
 
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+    }
+  }, [auth, authError]);
   return (
     <AuthForm
       formType="signUp"
