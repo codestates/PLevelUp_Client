e71c9b62a923ec33d506b805029c4cfe392d9182
Napsztar <napsztar@gmail.com>
2021-04-07 13:45:20 +0900

    Add store to Thunk

--- src/index.tsx
+++ src/index.tsx
@@ -5,11 +5,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { applyMiddleware,createStore } from 'redux';
 import rootReducer from './modules';
+import ReduxThunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const store = createStore(rootReducer, composeWithDevTools());
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 ReactDOM.render(
   <React.StrictMode>
