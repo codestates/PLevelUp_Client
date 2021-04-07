065f316efcb2a5824fb9764fa24dd9011aaf95cd
Napsztar <napsztar@gmail.com>
2021-04-07 14:21:44 +0900

    Update reducerUtils change error type

--- src/lib/reducerUtils.ts
+++ src/lib/reducerUtils.ts
@@ -1,26 +1,29 @@
+import { AxiosError } from 'axios';
+
 export type AsyncState<T, E = any> = {
   data: T | null;
   loading: boolean;
-  error: E | null;
+  error: AxiosError | null;
 };
 
 export const asyncState = {
-  initial: <T, E = any>(initialData?: T): AsyncState<T, E> => ({
+  // 다음 코드는 화살표 함수에 Generic 을 설정 한 것입니다.
+  initial: <T, E = any>(initialData?: T): AsyncState<T, AxiosError> => ({
     loading: false,
     data: initialData || null,
     error: null,
   }),
-  load: <T, E = any>(data?: T): AsyncState<T, E> => ({
+  load: <T, E = any>(data?: T): AsyncState<T, AxiosError> => ({
     loading: true,
     data: data || null,
     error: null,
   }),
-  success: <T, E = any>(data: T): AsyncState<T, E> => ({
+  success: <T, E = any>(data: T): AsyncState<T, AxiosError> => ({
     loading: false,
     data,
     error: null,
   }),
-  error: <T, E>(error: E): AsyncState<T, E> => ({
+  error: <T, E>(error: AxiosError): AsyncState<T, AxiosError> => ({
     loading: false,
     data: null,
     error: error,
