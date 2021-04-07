4877e8ff2476216bd426cf9df6758ae824281561
Napsztar <napsztar@gmail.com>
2021-04-07 16:52:19 +0900

    Update header

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 import Responsive from './Responsive';
 import Button from './Button';
-import { MasterIsLoginResType } from '../../api/master/auth';
 
 const HeaderBlock = styled.div`
   position: fixed;
@@ -10,14 +9,11 @@ const HeaderBlock = styled.div`
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.08);
 `;
 
-/**
- * Responsive 컴포넌트의 속성에 스타일을 추가해서 새로운 컴포넌트 생성
- */
 const Wrapper = styled(Responsive)`
   height: 4rem;
   display: flex;
   align-items: center;
-  justify-content: space-between; /* 자식 엘리먼트 사이의 여백을 최대로 설정 */
+  justify-content: space-between;
 
   .logo {
     font-size: 1.125rem;
@@ -31,9 +27,6 @@ const Wrapper = styled(Responsive)`
   }
 `;
 
-/**
- 헤더가 fixed로 되어 있기 때문에 페이지의 콘텐츠가 4rem 아래에 나타나도록 해주는 컴포넌트
- */
 const Spacer = styled.div`
   height: 4rem;
 `;
