96b9a669c5d65cb736e9c529aa005a89c4d9e205
Napsztar <napsztar@gmail.com>
2021-04-11 18:30:22 +0900

    Remove LandingPage

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,16 @@ import { masterIsLoginThunk, masterTempSetUser } from './modules/master/user';
 import { mainIsLoginThunk, mainTempSetUser } from './modules/main/user';
 
 import ModifyUserInfo from './pages/Modify_userInfoPage';
-import LandingPage from './pages/LandingPage';
 import ListPage from 'pages/ListPage';
 import DetailPage from 'pages/DetailPage';
-import IntronducePage from 'pages/IntroducePage';
+import IntroducePage from 'pages/IntroducePage';
 import PaymentPage from 'pages/PaymentPage';
 import LoginPage from 'pages/LoginPage';
 import SignUpPage from 'pages/SignUpPage';
 import MyPage from 'pages/MyPage';
 import Footer from 'components/common/Footer';
 import Header from 'components/common/Header';
-import ScrollToTop from 'containers/commom/ScrollToTop';
+import ScrollToTop from 'containers/common/ScrollToTop';
 import MasterLoginPage from './pages/master/LoginPage';
 import MasterSignUpPage from './pages/master/SignUpPage';
 
@@ -45,15 +44,15 @@ function App() {
       <ScrollToTop />
       <Header />
       <Switch>
-        {/* <Route exact path="/master/login" component={MasterLoginPage} />
-        <Route exact path="/master/sign-up" component={MasterSignUpPage} />
+        <Route exact path="/master/login" component={MasterLoginPage} />
+        <Route exact path="/master/signup" component={MasterSignUpPage} />
         <Route exact path="/list" component={ListPage} />
         <Route exact path="/detail" component={DetailPage} />
         <Route exact path="/payment" component={PaymentPage} />
-        <Route exact path="/introduce" component={IntronducePage} />
+        <Route exact path="/introduce" component={IntroducePage} />
         <Route exact path="/login" component={LoginPage} />
         <Route exact path="/signup" component={SignUpPage} />
-        <Route exact path="/mypage" component={MyPage} /> */}
+        <Route exact path="/mypage" component={MyPage} />
         <Route exact path="/" component={ModifyUserInfo} />
       </Switch>
       <Footer />
