72b1f38053ec784421761b119398add706240680
Napsztar <napsztar@gmail.com>
2021-04-12 17:32:18 +0900

    Add ClubReadPage to pages directory

---
+++ src/pages/master/ClubReadPage.tsx
@@ -0,0 +1,11 @@
+import HeaderContainer from '../../containers/common/HeaderContainer';
+import Viewer from '../../components/master/club/read/Viewer';
+
+export default function ClubReadPage() {
+  return (
+    <>
+      <HeaderContainer />
+      <Viewer />
+    </>
+  );
+}
