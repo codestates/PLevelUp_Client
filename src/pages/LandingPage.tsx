acda6ec81fa96c34ef3d469509a7b6de8bbbe757
Outwater <kocon135@gmail.com>
2021-04-14 02:10:24 +0900

    Implement slick banner

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from '../styles/pages/landing_page/LandingPage.module.scss';
-
+import SlickBannerContainer from '../containers/slick/SlickBannerContainer';
+import SlickClubListContainer from '../containers/slick/SlickClubListContainer';
 const LandingPage = () => {
   return (
     <div className={styles.wrapper}>
-      <h1> 랜딩 페이지 </h1>
-      <div className={styles.banner}> </div>
+      <SlickBannerContainer />
+      <SlickClubListContainer />
     </div>
   );
 };
