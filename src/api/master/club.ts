ab8ed7c6cd12f9eb6899a2fe3de5785469411cf7
Napsztar <napsztar@gmail.com>
2021-04-13 15:12:09 +0900

    Add API masterClubRead

--- src/api/master/club.ts
+++ src/api/master/club.ts
@@ -27,36 +27,59 @@ export type MasterClubEditResType = {
   day: string;
   limitUserNumber: number;
   createdAt: Date;
-  updatedAt: Date | null;
+  updatedAt?: Date | null;
   MasterId: number;
 };
 
 export async function masterClubWrite({
-title,
-summary,
-price,
-place,
-description,
-topic,
-startDate,
-endDate,
-day,
-limitUserNumber
-                                  }: MasterClubEditReqType) {
-  const response = await api.post<MasterClubEditResType>(
-    `/api/master/club`,
-    {
-      title,
-summary,
-price,
-place,
-description,
-topic,
-startDate,
-endDate,
-day,
-limitUserNumber
-    },
-  );
+  title,
+  summary,
+  price,
+  place,
+  description,
+  topic,
+  startDate,
+  endDate,
+  day,
+  limitUserNumber,
+}: MasterClubEditReqType) {
+  const response = await api.post<MasterClubEditResType>(`/api/master/club`, {
+    title,
+    summary,
+    price,
+    place,
+    description,
+    topic,
+    startDate,
+    endDate,
+    day,
+    limitUserNumber,
+  });
   return response.data;
 }
+
+type MasterClubReadMasterType = {
+  id: number;
+  email: string;
+  username: string;
+};
+
+export type MasterClubReadResType = {
+  id: number;
+  title: string;
+  summary: string;
+  place: string;
+  price: number;
+  description: string;
+  topic: string;
+  startDate: Date;
+  endDate: Date;
+  day: string;
+  limitUserNumber: number;
+  createdAt: Date;
+  updatedAt?: Date | null;
+  MasterId: number;
+  Master: MasterClubReadMasterType;
+};
+export const masterClubRead = (id: number) =>
+  api.get<MasterClubReadResType>(`/api/master/club/${id}`);
