e1e9b9c9bc0b781b3dbd9f915ce0be0e71c49608
Napsztar <napsztar@gmail.com>
2021-04-13 15:56:43 +0900

    Add list module

---
+++ src/modules/master/club/list.ts
@@ -0,0 +1,63 @@
+import createAsyncThunk, {
+  createRequestActionTypes,
+} from '../../../lib/createAsyncThunk';
+import { ActionType, createAsyncAction, createReducer } from 'typesafe-actions';
+import {
+  masterClubList,
+  MasterClubListResType,
+} from '../../../api/master/club';
+import { AxiosError } from 'axios';
+import { AsyncState, asyncState } from '../../../lib/reducerUtils';
+
+// async 액션 타입
+const [
+  MASTER_CLUB_LIST,
+  MASTER_CLUB_LIST_SUCCESS,
+  MASTER_CLUB_LIST_FAILURE,
+] = createRequestActionTypes('master-list/MASTER_CLUB_LIST'); // 클럽 목록 조회
+
+// async 생성 함수
+export const masterClubListAsync = createAsyncAction(
+  MASTER_CLUB_LIST,
+  MASTER_CLUB_LIST_SUCCESS,
+  MASTER_CLUB_LIST_FAILURE,
+)<any, [MasterClubListResType], AxiosError>();
+
+// async 액션
+const asyncActions = {
+  masterClubListAsync,
+};
+type ListAsyncAction = ActionType<typeof asyncActions>;
+
+type ListAsyncState = {
+  clubs: AsyncState<MasterClubListResType, Error>;
+};
+
+const asyncInitialState: ListAsyncState = {
+  clubs: asyncState.initial(),
+};
+
+export const masterListAsync = createReducer<ListAsyncState, ListAsyncAction>(
+  asyncInitialState,
+  {
+    [MASTER_CLUB_LIST]: state => ({
+      ...state,
+      clubs: asyncState.load(),
+    }),
+    [MASTER_CLUB_LIST_SUCCESS]: (state, action) => ({
+      ...state,
+      clubs: asyncState.success(action.payload),
+    }),
+    [MASTER_CLUB_LIST_FAILURE]: (state, action) => ({
+      ...state,
+      clubs: asyncState.error(action.payload),
+    }),
+  },
+);
+
+export default masterListAsync;
+
+export const masterListThunk = createAsyncThunk(
+  masterClubListAsync,
+  masterClubList,
+);
