a2b80d204d71ecaabebcaac72dc2de4f0fdfcc18
Outwater <kocon135@gmail.com>
2021-04-13 13:07:35 +0900

    Add landingpage

---
+++ src/pages/LandingPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from '../styles/pages/landing_page/LandingPage.module.scss';
+
+const LandingPage = () => {
+  return (
+    <div className={styles.wrapper}>
+      <h1> 랜딩 페이지 </h1>
+      <div className={styles.banner}> </div>
+    </div>
+  );
+};
+
+export default LandingPage;
