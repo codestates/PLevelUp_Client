ebb8851161d3b9fb8c6c14f406e86f1256a4a9bd
Outwater <kocon135@gmail.com>
2021-04-14 12:25:03 +0900

    Fix arrowBtn

--- src/components/slick/SlickBanner.tsx
+++ src/components/slick/SlickBanner.tsx
@@ -2,8 +2,40 @@ import React, { Component } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+
 import styles from '../../styles/pages/landing_page/SlickBanner.module.scss';
 import { Link } from 'react-router-dom';
+
+function SampleNextArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        right: '3%',
+        zIndex: 1,
+      }}
+      onClick={onClick}
+    />
+  );
+}
+function SamplePrevArrow(props: any) {
+  const { className, style, onClick } = props;
+  return (
+    <div
+      className={className}
+      style={{
+        ...style,
+        display: 'block',
+        left: '3%',
+        zIndex: 1,
+      }}
+      onClick={onClick}
+    />
+  );
+}
 
 export default function SlickBanner({ data }: any) {
   const settings = {
@@ -18,6 +50,8 @@ export default function SlickBanner({ data }: any) {
     speed: 2000,
     autoplaySpeed: 5000,
     cssEase: 'linear',
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
   return (
     <div className={styles.container}>
