f352b0799dfb6719edc8a66e681957ff0b8b7354
Napsztar <napsztar@gmail.com>
2021-04-16 02:40:23 +0900

    Implement infinite scroll to club list

--- src/components/club/list/List.tsx
+++ src/components/club/list/List.tsx
@@ -6,24 +6,14 @@ import {
 } from '../../../api/main/club';
 import styles from '../../../styles/pages/list_page/ListPage.module.scss';
 import ClubCard from '../../common/ClubCard';
-import React from 'react';
-
-type ListItemType = {
-  club: MainClubReadResType;
-};
+import React, { memo } from 'react';
 
 type ListType = {
-  clubs: MainClubListResType | null;
-  error: AxiosError | null;
-  loading: boolean;
-  bookmark: any;
+  clubs: MainClubListResType;
+  bookmark: any | null;
 };
 
-export default function List({ clubs, loading, error, bookmark }: ListType) {
-  if (error) return <div>Oops..? 알수 없는 에러가 발생했나봐요..</div>;
-
-  if (loading || !clubs) return <div>로딩중 ..</div>;
-
+export default function List({ clubs, bookmark }: ListType) {
   return (
     <div className={styles.wrapper}>
       <h1>리스트페이지 </h1>
