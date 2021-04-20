5b68b7a2bad11e2ae0d4c431f109f23ae2f44fc1
Outwater <kocon135@gmail.com>
2021-04-21 04:17:07 +0900

    Fix detail-card attributes

--- src/containers/club/read/ViewerContainer.tsx
+++ src/containers/club/read/ViewerContainer.tsx
@@ -1,24 +1,40 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { withRouter } from 'react-router-dom';
 import { RootState } from '../../../modules';
 import {
   mainClubReadThunk,
   mainClubUnloadRead,
 } from '../../../modules/club/read';
+import {
+  addBookmarkThunk,
+  removeBookmarkThunk,
+} from '../../../modules/club/bookmark';
 import Viewer from '../../../components/club/read/Viewer';
 
 export default withRouter(function ViewerContainer({ match, history }) {
   // 처음 마운트 될 떄 클럽 읽기 API 요청
   const { clubId } = match.params;
   const dispatch = useDispatch();
-  const { data: club, error, loading } = useSelector(
-    ({ mainReadAsync }: RootState) => ({
-      data: mainReadAsync.club.data,
+  const { data: club, error, loading, bookmark } = useSelector(
+    ({ mainReadAsync, mainBookmarkAsync }: RootState) => ({
+      data: mainReadAsync.club.data!,
       error: mainReadAsync.club.error,
       loading: mainReadAsync.club.loading,
+      bookmark: mainBookmarkAsync.bookmark.data!,
     }),
   );
+  const onAddBookmark = () => {
+    dispatch(addBookmarkThunk(club.id));
+  };
+  const onRemoveBookmark = () => {
+    dispatch(removeBookmarkThunk(club.id));
+  };
+  const [isBookmarked, setIsBookmarked] = useState(club?.isBookmark);
+
+  useEffect(() => {
+    setIsBookmarked(bookmark?.isBookmark);
+  }, [bookmark]);
 
   useEffect(() => {
     dispatch(mainClubReadThunk(clubId));
@@ -28,5 +44,14 @@ export default withRouter(function ViewerContainer({ match, history }) {
     };
   }, []);
 
-  return <Viewer club={club} loading={loading} error={error} />;
+  return (
+    <Viewer
+      club={club}
+      onAddBookmark={onAddBookmark}
+      onRemoveBookmark={onRemoveBookmark}
+      isBookmarked={isBookmarked}
+      loading={loading}
+      error={error}
+    />
+  );
 });
