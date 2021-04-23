1bd5c7c10d9e228273bb97eede352cf161345941
bribrie <ibriebriee@gmail.com>
2021-04-23 21:33:53 +0900

    Update footer link

--- src/components/my-page/BookmarkList.tsx
+++ src/components/my-page/BookmarkList.tsx
@@ -8,7 +8,6 @@ type BookmarkListPropsType = {
 export default function BookmarkList({ clubs }: BookmarkListPropsType) {
   return (
     <div className={styles.wrapper}>
-      <div className={styles.title}> 찜리스트</div>
       <div className={styles.container}>
         {clubs?.map((club: MainClubReadResType) => {
           return <ClubCardContainer club={club} key={club.id} isMain={true} />;
