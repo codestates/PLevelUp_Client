fc9f2c137ff90653b4148fb748b8de7351a129cd
Napsztar <napsztar@gmail.com>
2021-04-25 03:02:00 +0900

    Fix lowerCase -> UpperCase component Name

--- src/components/club/read/ApplyFAQList.tsx
+++ src/components/club/read/ApplyFAQList.tsx
@@ -3,8 +3,7 @@ import ApplyFAQItem from './ApplyFAQItem';
 import styles from '../../../styles/pages/read_page/ReadPage.module.scss';
 
 //faq area
-export default function faqArea({ faq }: { faq: any }) {
-
+export default function FaqArea({ faq }: { faq: any }) {
   const [faqData, setFaqData] = useState(faq);
 
   return (
@@ -12,16 +11,11 @@ export default function faqArea({ faq }: { faq: any }) {
       <div className={styles.faqContainer}>
         <div className={styles.faqContents}>
           <div className={styles.faqTitle}>FAQ</div>
-          {
-            faqData.map((data: any) => (
-              <ApplyFAQItem key={data.id} faq={data} />
-            ))
-          }
-
+          {faqData.map((data: any) => (
+            <ApplyFAQItem key={data.id} faq={data} />
+          ))}
         </div>
       </div>
     </div>
   );
-
-
 }
