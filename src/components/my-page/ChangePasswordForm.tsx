642b89d82ffa504d936599a0b369bccc8704ba38
Napsztar <napsztar@gmail.com>
2021-04-25 23:56:12 +0900

    Refactor ChangePasswordForm

--- src/components/my-page/ChangePasswordForm.tsx
+++ src/components/my-page/ChangePasswordForm.tsx
@@ -18,32 +18,44 @@ export default function ChangePasswordForm({
   return (
     <div className={styles.passwordWrapper}>
       <div className={styles.changePasswordHeader}>비밀번호 변경</div>
+      <hr className={styles.hr} />
       <div className={styles.changePasswordContent}>
         <form className={styles.formGroup} onSubmit={onSubmit}>
-          <input
-            className={styles.basicInput}
-            name="password"
-            type="password"
-            placeholder="기존 비밀번호"
-            onChange={onChange}
-            value={form.password}
-          />
-          <input
-            className={styles.basicInput}
-            name="changePassword"
-            type="password"
-            placeholder="새 비밀번호"
-            onChange={onChange}
-            value={form.changePassword}
-          />
-          <input
-            className={styles.basicInput}
-            name="changePasswordConfirm"
-            type="password"
-            placeholder="새 비밀번호 확인"
-            onChange={onChange}
-            value={form.changePasswordConfirm}
-          />
+          <div className={styles.inputBox}>
+            <span className={styles.label}>
+              기존 비밀번호<span className={styles.required}>*</span>
+            </span>
+            <input
+              className={styles.basicInput}
+              name="password"
+              type="password"
+              placeholder="기존 비밀번호을 입력해주세요."
+              onChange={onChange}
+              value={form.password}
+            />
+            <span className={styles.label}>
+              새 비밀번호<span className={styles.required}>*</span>
+            </span>
+            <input
+              className={styles.basicInput}
+              name="changePassword"
+              type="password"
+              placeholder="새 비밀번호를 입력해주세요."
+              onChange={onChange}
+              value={form.changePassword}
+            />
+            <span className={styles.label}>
+              새 비밀번호 확인<span className={styles.required}>*</span>
+            </span>
+            <input
+              className={styles.basicInput}
+              name="changePasswordConfirm"
+              type="password"
+              placeholder="새 비밀번호를 다시 입력해주세요"
+              onChange={onChange}
+              value={form.changePasswordConfirm}
+            />
+          </div>
           <button className={styles.changeBtn}>변경</button>
           {error && <div className={styles.errorMessage}>{error}</div>}
         </form>
