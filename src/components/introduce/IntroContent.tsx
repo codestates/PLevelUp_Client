510ecbbadd48d61585b33d0e3f5c1d9eac8afe99
bribrie <ibriebriee@gmail.com>
2021-04-26 02:24:18 +0900

    Impelement introduce page & css

--- src/components/introduce/IntroContent.tsx
+++ src/components/introduce/IntroContent.tsx
@@ -1,15 +1,19 @@
 import styles from '../../styles/pages/introduce_page/IntroducePage.module.scss';
+import introContent from '../../asset/IntroContent/IntroContent.png';
 
 export default function IntroduceContent() {
   return (
     <div>
-      <h1>P' Level Up</h1>
-      <div>
-        P' Level Up 프레벨업은 Programming Level Up, 프로그래밍 실력을 레벨 업
-        할 수 있도록 환경을 제공하는 서비스입니다.
+      <div className={styles.wrapper}>
+        <div className={styles.contentContainer}>
+          <hr />
+          <img
+            className={styles.contentImg}
+            src={introContent}
+            alt="introduce"
+          ></img>
+        </div>
       </div>
-      <h1>아지트 설명</h1>
-      <div></div>
     </div>
   );
 }
